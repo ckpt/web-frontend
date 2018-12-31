890bb9c804b740ee7d74b82d77308cbb49aaeb69
Morten Knutsen <morten.knutsen@uninett.no>
2018-12-31 10:50:47 +0100

    Move to webpack

---
+++ assets.js
@@ -0,0 +1,16 @@
+const CSS = [
+    'bootstrap/dist/css/bootstrap.min.css',
+    'metismenu/dist/metisMenu.min.css',
+    'font-awesome/css/font-awesome.min.css'
+];
+const JS = [
+    'bootstrap/dist/js/bootstrap.min.js',
+    'jquery/dist/jquery.min.js',
+    'metismenu/dist/metisMenu.min.js',
+    'raphael/raphael.min.js',
+];
+
+module.exports = {
+    JS: JS,
+    CSS: CSS,
+};
