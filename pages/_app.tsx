8555714a1ecef97f401a491c437577960852f9e7
Morten Knutsen <mortenk@Mortens-MacBook-Pro.local>
2023-05-20 14:37:40 +0200

    Adjust skeleton to fit CKPT

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AuthProvider, ProtectRoute } from '@/lib/contexts/auth'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
+export default function App({ Component, pageProps, router }: AppProps) {
+  return (
+    <AuthProvider>
+      <ProtectRoute>
+        <Component {...pageProps} />
+      </ProtectRoute>
+    </AuthProvider>
+  )
 }
